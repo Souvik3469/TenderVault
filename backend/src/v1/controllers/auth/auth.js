d633dbf3e8afb20f4e924867b6fbdc73a2ccfd93
Souvik3469 <souviksen3469@gmail.com>
2023-10-02 12:54:19 +0530

    Signup Login backend

--- backend/src/v1/controllers/auth/auth.js
+++ backend/src/v1/controllers/auth/auth.js
@@ -9,6 +9,88 @@ import { customResponse } from "../../../utils/Response";
 
 const prisma = new PrismaClient();
 const loginController = {
-  
+  async login(req, res, next) {
+    try {
+      const { email, password } = await req.body;
+      let user;
+      user = await prisma.user.findUniqueOrThrow({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        return next(createError.Unauthorized("Verify your Credentials"));
+      }
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        return next(createError.Unauthorized("Verify your Credentials1"));
+      }
+
+      // generating jwt tokens for the logged in user
+
+      const accessToken = jwt.sign(user.id, process.env.USER_ACCESS_SECRET);
+
+      res.cookie("accessToken", accessToken, {
+        maxAge: ms("30m"),
+        httpOnly: true,
+      });
+
+      res.json(customResponse(200, { accessToken }));
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: err,
+      });
+    }
+  },
+
+  async logout(req, res, next) {
+    try {
+      res.clearCookie("accessToken");
+      res.json(customResponse(200, "Logged Out"));
+    } catch (err) {
+      console.log(err);
+      return next(createError.InternalServerError());
+    }
+  },
+  // register function
+
+  async register(req, res, next) {
+    try {
+      // name , password , phonenumber , email -> for register
+
+      const resp = await req.body;
+      delete resp.confirmPassword;
+      const user = await prisma.user.findUnique({
+        where: {
+          email: resp.email, // finds the user with the given mail
+        },
+      });
+      if (user) {
+        res.status(400).json({
+          message: "User already exists",
+        });
+      }
+      // normal salting and hashing password
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(resp.password, salt);
+      const data = {
+        ...resp,
+        password: hashedPassword,
+      };
+      const createduser = await prisma.user.create({
+        data,
+      });
+      res.status(200).json({
+        message: "User created successfully",
+        createduser,
+      });
+    } catch (err) {
+      console.log(err);
+      res.json(customResponse(400, err));
+    }
+  },
 };
 export default loginController;
--- backend/src/v1/routes/Auth.route.js
+++ backend/src/v1/routes/Auth.route.js
@@ -6,7 +6,7 @@ import authMiddleware from "../middlewares/Auth.middleware";
 
 const router = express.Router();
 
-// router.post("/login", loginController.login);
-// router.post("/register", loginController.register);
-// router.post("/logout", authMiddleware, loginController.logout);
+router.post("/login", loginController.login);
+router.post("/register", loginController.register);
+router.post("/logout", authMiddleware, loginController.logout);
 export default router;
