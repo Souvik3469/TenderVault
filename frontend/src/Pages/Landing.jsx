1564ecf0b696bb83101b85e9b7ccc6865eca5ed0
Souvik3469 <souviksen3469@gmail.com>
2023-10-16 03:17:00 +0530

    Frontend Auth integrated

---
+++ frontend/src/Pages/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className=" bg-red-600">Landing</div>
+  )
+}
+
+export default Landing
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import App from "./App";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster, toast, useToasterStore } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 3000, // 5 minutes
+    },
+  },
+});
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <BrowserRouter>
+        <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+        toastOptions={{ duration: 5000 }}
+      />
+        <App />
+        </BrowserRouter>
+      </LocalizationProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
